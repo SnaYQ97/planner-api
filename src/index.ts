d8a5f9c490a768928bd509eeba6f699d4a9474d6
snayq <artowir9@wp.pl>
2024-07-21 00:19:18 +0200

    fixing login

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import express, { json, urlencoded } from "express";
+import express, {json, urlencoded, Request, Response, NextFunction} from "express";
 import cors, {CorsOptions} from "cors";
 import session from "express-session";
-
-
-import usersRouter from "./routes/users.router";
 import authRouter from "./routes/auth.router";
+import passport from "passport";
+import {PrismaClient} from "@prisma/client";
+import * as passportStrategy from "passport-local";
+import {pbkdf2, randomBytes, timingSafeEqual} from "node:crypto";
+import router from "./routes/users.router";
 
 const app = express();
 const PORT = 3000;
@@ -13,7 +15,7 @@ const second = 1000;
 enum Path {
   HOME = '/',
   AUTH = '/auth',
-  USERS = '/user',
+  USER = '/user',
 }
 
 app.use(
@@ -21,28 +23,110 @@ app.use(
   urlencoded({ extended: true }),
   session({
     secret: 'keyboard cat',
-    saveUninitialized: true,
+    saveUninitialized: false,
     resave: false,
-    cookie: {
-      maxAge: second * secondsInMinute * 60, // 1 hour
-      secure: true,
-    }
   }),
 );
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 const corsOptions: CorsOptions = {
   origin: [`http://localhost:${PORT}`, "http://localhost:5173"],
   optionsSuccessStatus: 200,
 }
 
-app.get(Path.HOME, cors(corsOptions), async (request, res) => {
+app.get(Path.HOME, cors(corsOptions), async (req, res) => {
   console.log('home called');
   res.status(200).send('Hello World!');
 });
 
-
-
-app.use(Path.USERS, cors(corsOptions), usersRouter);
 app.use(Path.AUTH, cors(corsOptions), authRouter);
+
+const prisma = new PrismaClient();
+
+passport.use(new passportStrategy.Strategy(async (email, password, done) => {
+    prisma.user.findUnique({
+        where: {
+            email: email,
+        },
+    }).then((result) => {
+        if (!result) {
+            return done(null, false, { message: 'User not found'});
+        }
+        pbkdf2(password, result.salt, 31000, 32, 'sha256', (err, hashedPassword) => {
+            if (err) {
+                return done(err);
+            }
+            if (!timingSafeEqual(result.password, hashedPassword)) {
+                return done(null, false, { message: 'Incorrect password' });
+            }
+            const { password, ...user } = result;
+            return done(null, user);
+        });
+    }).catch((err) => {
+        return done(err);
+    });
+}));
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser<string>((id, done) => {
+    prisma.user.findUnique({
+        where: {
+            id: id,
+        },
+    }).then((user) => {
+        done(null, user);
+    }).catch((err) => {
+        done(err);
+    });
+});
+
+router.post(`${Path.USER}/create`, (req, res, next) => {
+    const salt = randomBytes(32);
+    pbkdf2(req.body.password, salt, 31000, 32, 'sha256', async (err,  hashedPassword) => {
+        if (err) return next(err);
+        await prisma.user.create({
+            data: {
+                email: req.body.email,
+                password: hashedPassword,
+                salt: salt,
+            }
+        }).then((user) => {
+            res.send('User created');
+        }).catch((err) => next(err));
+    });
+});
+
+app.get(`${Path.USER}/fail`, async (req, res) => {
+    res.send('Failed to login');
+});
+
+app.post(`${Path.USER}/login`, passport.authenticate('local', {
+    failureMessage: true,
+    successRedirect: `${Path.USER}/secured`,
+    failureRedirect: `${Path.USER}/fail`,
+}));
+
+const ensureAuthenticated = (req: Request, res: Response, next: NextFunction) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.send('Unauthorized');
+}
+
+app.get(`${Path.USER}/secured`, ensureAuthenticated, (req, res) => {
+    res.send('Secured');
+});
+
+app.get(`${Path.USER}/logout`, (req, res) => {
+    req.logout((err) => {
+        if(err) return res.send(err);
+        return res.send('Logged out');
+    });
+});
 
 export default app;
