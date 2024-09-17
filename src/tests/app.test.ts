9e7c626b743357c7f64d0fb89c6091135f30b8d9
Artur <artowir9@wp.pl>
2024-09-17 23:28:42 +0200

    8 - Add testing library
    Rewrite jest configuration to javascript
    
    #8

--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 
 import app from "../index";
+import exp from "node:constants";
 
 describe('test api', () => {
   it('should return 200', async () => {
@@ -13,5 +14,17 @@ describe('GET /auth', () => {
   it('should return 401', async () => {
     const response = await request(app).get('/auth/status');
     expect(response.statusCode).toBe(401);
+  });
+});
+
+describe('User creation', () => {
+
+  it('', async () => {
+    const response = await request(app).post('/user/create').send({
+      email: 'test@test.test',
+      password: 'test1234',
+      passwordConfirmation: 'test1234'
+    });
+    expect(response.statusCode).toBe(201);
   });
 });
